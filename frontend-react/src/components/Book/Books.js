9caaa40caa43cca22e68d1858a9b6154467eb84b
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 18:05:01 +0600

    Rendering all books info

--- frontend-react/src/components/Book/Books.js
+++ frontend-react/src/components/Book/Books.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import Book from './Book'
 import axios from 'axios'
 
 const URL = "http://localhost:5000/books"
@@ -8,13 +9,19 @@ const fetchHandler = async () => {
 const Books = () => {
     const [books,setBooks]=useState()
     useEffect(() => {
-            fetchHandler().then(data=>setBooks(data))
+            fetchHandler().then(data=>setBooks(data.books))
         },[]
     )
     console.log(books)
     return (
         <div>
-            All books
+            <ul>
+                {books && books.map((book,i)=>(
+                    <div key={i}>
+                        <Book book={book}/>
+                    </div>
+                ))}
+            </ul>
         </div>
     );
 };
