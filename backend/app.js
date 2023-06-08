71b9ce64a1ff3d1aeec29a2021f85f101465a361
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 17:55:24 +0600

    fetching books data from the backend and database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const router = require("./routes/book-routes")
+const cors = require('cors')
 
 const app = express()
 // middlewares
 app.use(express.json())
+app.use(cors())
 app.use("/books",router) // localhost:5000/books
 
 mongoose.connect("mongodb+srv://admin:fh9f5ZGXZDIQop2O@cluster0.gojklws.mongodb.net/?retryWrites=true&w=majority")
--- frontend-react/src/components/Book/Books.js
+++ frontend-react/src/components/Book/Books.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
 
+const URL = "http://localhost:5000/books"
+const fetchHandler = async () => {
+    return await axios.get(URL).then((res) => res.data)
+}
 const Books = () => {
+    const [books,setBooks]=useState()
+    useEffect(() => {
+            fetchHandler().then(data=>setBooks(data))
+        },[]
+    )
+    console.log(books)
     return (
         <div>
             All books
