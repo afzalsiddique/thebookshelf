ad3deeb48c7f87d78869dd12dc2d6ca887a1e1a9
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 16:56:53 +0600

    created navbar and home route

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-    </div>
+      <React.Fragment>
+          <header>
+              <Header />
+          </header>
+          <main>
+              <Routes>
+                  <Route path="/" element={<Home />} exact />
+              </Routes>
+          </main>
+      </React.Fragment>
+
   );
 }
 
---
+++ frontend-react/src/components/Header.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import {AppBar, Tabs, Tab, Toolbar, Typography} from '@mui/material'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+    const [value, setValue] = useState()
+    return <div>
+        <AppBar position="sticky">
+            <Toolbar>
+                <Typography>
+                    <LibraryBooksIcon/>
+                </Typography>
+                <Tabs textColor="inherit" indicatorColor="secondary" value={value} onChange={(e,val)=>setValue(val)}>
+                    <Tab LinkComponent={NavLink} to='/add' label='Add Book'/>
+                    <Tab LinkComponent={NavLink} to='/books' label='Books'/>
+                    <Tab LinkComponent={NavLink} to='/about' label='About Us'/>
+                </Tabs>
+            </Toolbar>
+        </AppBar>
+    </div>
+}
+
+export default Header
