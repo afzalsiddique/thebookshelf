576188463e42ebe277e2e148f8baa6c287df422e
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 12:19:30 +0600

    create books controller

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,19 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Book = require("../model/Book")
+const booksController = require("../controllers/books-controller")
 
-router.get("/",async (req,res,next)=>{
-    // get all books
-    let books
-    try{
-        books = await Book.find()
-    }catch (err){
-        console.log(err)
-    }
-    if (!books){
-        return res.status(404).json({message:"No products found"})
-    }
-    return res.status(200).json({books})
-})
+router.get("/", booksController.getAllBooks)
 
 module.exports = router
