42c803269995595e2a4a72526db06081c31b0294
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 14:05:54 +0600

    delete book by id

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -63,7 +63,22 @@ const updateBook = async(req,res,next)=>{
     return res.status(200).json({book})
 }
 
+const deleteBook = async(req,res,next)=>{
+    const id = req.params.id
+    let book
+    try{
+        book = await Book.findByIdAndRemove(id)
+    } catch (err) {
+        console.log(err)
+    }
+    if (!book){
+        return res.status(404).json({message:"Unable to delete by this id"})
+    }
+    return res.status(200).json({message:"Books successfully deleted"})
+}
+
 exports.getAllBooks = getAllBooks
 exports.addBook = addBook
 exports.getById = getById
 exports.updateBook = updateBook
+exports.deleteBook = deleteBook
