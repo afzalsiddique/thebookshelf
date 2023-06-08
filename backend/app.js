af44f2b9b266487a37e5e6d7541c243935e69b36
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 14:19:37 +0600

    created a folder 'backend' (2)

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const router = require("./routes/book-routes")
+
+const app = express()
+// middlewares
+app.use(express.json())
+app.use("/books",router) // localhost:5000/books
+
+mongoose.connect("mongodb+srv://admin:fh9f5ZGXZDIQop2O@cluster0.gojklws.mongodb.net/?retryWrites=true&w=majority")
+    .then(()=> console.log("Connected to mongodb"))
+    .then(()=>{
+        app.listen(5000)
+    }).catch((err)=>console.log(err))
