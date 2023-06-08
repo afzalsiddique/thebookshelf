c05fcdc0e4d56e9072c1f94af95a465eadebc792
afzalsiddique <afzal@intelligentmachin.es>
2023-06-08 12:03:00 +0600

    Book Schema created

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world!!")
+const express = require("express")
+const mongoose = require("mongoose")
+const router = require("./routes/book-routes")
+
+const app = express()
+// middlewares
+app.use("/books",router) // localhost:5000/books
+
+mongoose.connect("mongodb+srv://admin:fh9f5ZGXZDIQop2O@cluster0.gojklws.mongodb.net/?retryWrites=true&w=majority")
+    .then(()=> console.log("Connected to mongodb"))
+    .then(()=>{
+        app.listen(5000)
+    }).catch((err)=>console.log(err))
